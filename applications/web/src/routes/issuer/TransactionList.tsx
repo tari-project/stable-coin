7772e4b4a318463618d55963d59236d4b3782a8d
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2024-04-10 17:28:35 +0100

    show transaction info

--- applications/web/src/routes/issuer/TransactionList.tsx
+++ applications/web/src/routes/issuer/TransactionList.tsx
@@ -3,13 +3,14 @@
 
 import * as React from "react";
 import { StyledPaper } from "../../components/StyledComponents.ts";
-import { Alert } from "@mui/material";
+import { Alert, IconButton, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import useTariProvider from "../../store/provider.ts";
 import { useNavigate } from "react-router-dom";
 import { StableCoinIssuer } from "../../store/stableCoinIssuer.ts";
 import Box from "@mui/material/Box";
 import { useEffect } from "react";
 import { utils } from "@tariproject/tarijs";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const INDEXER_ADDRESS = import.meta.env.VITE_INDEXER_ADDRESS;
 
@@ -23,6 +24,7 @@ function TransactionList({ issuer }: Props) {
   const [error, setError] = React.useState<Error | null>(null);
   const [invalid, setInvalid] = React.useState<any>({});
   const [busy, setBusy] = React.useState<{ [key: string]: boolean }>({});
+  const [transactions, setTransactions] = React.useState([]);
 
   if (!provider) {
     navigate("/");
@@ -52,13 +54,40 @@ function TransactionList({ issuer }: Props) {
         variables: {}
       })
     });
+
     let res_json = await res.json();
     console.log({ res_json });
     let events = res_json.data.getEvents;
-    events.forEach((event) => {
-      console.log(utils.toHexString(event.txHash));
+
+    let rows = events.map((event) => {
+      return {
+        tx_hash: utils.toHexString(event.txHash),
+        action: event.topic,
+        vault_id: event.payload.vault_id,
+        amount: event.payload.amount
+      };
     });
+    console.log({rows});
+    setTransactions(rows);
   }
+
+  function truncateText(text: string, length: number) {
+    if (!length || !text || text.length <= length) {
+        return text;
+    }
+    if (text.length <= length) {
+        return text;
+    }
+    const leftChars = Math.ceil(length/2);
+    const rightChars = Math.floor(length/2);
+    return text.substring(0, leftChars) + '...' + text.substring(text.length - rightChars);
+  }
+
+  async function handleCopyClick(text: string) {
+    if (text) {
+        navigator.clipboard.writeText(text);
+    }
+  };
 
   return (
     <StyledPaper sx={{ padding: 6 }}>
@@ -66,7 +95,39 @@ function TransactionList({ issuer }: Props) {
         <Box sx={{ paddingBottom: 4 }}>
           <Alert severity="error">{error.message}</Alert>
         </Box>
-      )}            
+      )}
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Transaction Hash</TableCell>
+            <TableCell>Vault Id</TableCell>
+            <TableCell>Action</TableCell>
+            <TableCell>Amount</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {transactions.map((row) => (
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell>
+                {truncateText(row.tx_hash, 20)}
+                <IconButton aria-label="copy" onClick={() => handleCopyClick(row.tx_hash)}>
+                    <ContentCopyIcon />
+                </IconButton>
+              </TableCell>
+              <TableCell>
+                {truncateText(row.vault_id, 20)}
+                <IconButton aria-label="copy" onClick={() => handleCopyClick(row.vault_id)}>
+                    <ContentCopyIcon />
+                </IconButton>
+              </TableCell>
+              <TableCell>{row.action}</TableCell>
+              <TableCell>{row.amount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>          
     </StyledPaper>
   );
 }
