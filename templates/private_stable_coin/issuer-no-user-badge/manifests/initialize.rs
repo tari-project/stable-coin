d07bde25c5bb9f8eb40bacf2f03a5b5e5c6116e2
Stan Bondi <sdbondi@users.noreply.github.com>
2026-04-07 17:42:55 +0400

    fix: fix init manifest

--- templates/private_stable_coin/issuer-no-user-badge/manifests/initialize.rs
+++ templates/private_stable_coin/issuer-no-user-badge/manifests/initialize.rs
@@ -1,14 +1,18 @@
 use template_xxx as StableCoin;
 
 pub fn main() {
+    let addr = new_component_addr!();
+    let account = var!["account"];
     // Initialize the StableCoin component
-    StableCoin::initialize(
+    let badge = StableCoin::instantiate(
+        addr,
         "100000000000000000000000000",
         "STC",
         Metadata("provider_name=StableCoin Inc."),
         8,
         // 32-byte public view key (hex)
-        PublicKey("deadbeaf..."),
+        PublicKey("6c64d361a661900be82695786a3f9cdbae26f7b216c3d093bea101a309700379"),
         true,
     );
+    account.deposit(badge);
 }
